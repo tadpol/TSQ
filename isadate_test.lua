4731ec6dd57bfe9553e855e8459a9dd81835dd06
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-07-01 15:06:00 -0500

    some nanosecond tests

--- isadate_test.lua
+++ isadate_test.lua
@@ -18,5 +18,13 @@ describe("Test the is_a_date function.", function()
 		assert.is_false(TSQ.is_a_date("1234-09-04T23:33:24"))
 	end)
 
+	it("validates dates with times and nanoseconds", function()
+		assert.is_true( TSQ.is_a_date("1234-10-12 12:33:24.2134"))
+		assert.is_true(TSQ.is_a_date("1234-09-04 23:33:24.2134"))
+		assert.is_true( TSQ.is_a_date("1234-10-12T12:33:24.2134Z"))
+		assert.is_true(TSQ.is_a_date("1234-09-04T23:33:24.2134Z"))
+		assert.is_false(TSQ.is_a_date("1234-09-04T23:33:24.2134"))
+	end)
+
 end)
 -- vim: set ai sw=4 ts=4 :
