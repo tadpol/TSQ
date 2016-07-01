b224a7d77133df878a83c9a075dcd193eee7ac7f
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-07-01 08:34:14 -0500

    test multiple equal series

--- results_test.lua
+++ results_test.lua
@@ -28,6 +28,42 @@ describe("Test the series results iterator", function()
 		assert.is_nil(t)
 	end)
 
+	it("can get the next value in multiple series", function()
+		local sr = {
+			series={
+				{
+					columns={"time","mean"},
+					name = "wintd",
+					values={{14673141, 77}, {14673142, 90} }
+				},
+				{
+					columns={"time","mean"},
+					name = "wintd",
+					values={{14673166, 66}, {14673167, 45} }
+				},
+			}
+		}
+		local i, t, u
+		i, t, u = TSQ.next_in_series(sr.series)
+		assert.are.equal(1, i)
+		assert.are.equal(t.time, 14673141)
+		assert.are.equal(t.mean, 77)
+		assert.are.equal(u.time, 14673166)
+		assert.are.equal(u.mean, 66)
+
+		i, t, u = TSQ.next_in_series(sr.series, 1)
+		assert.are.equal(2, i)
+		assert.are.equal(t.time, 14673142)
+		assert.are.equal(t.mean, 90)
+		assert.are.equal(u.time, 14673167)
+		assert.are.equal(u.mean, 45)
+
+		i, t, u = TSQ.next_in_series(sr.series, 2)
+		assert.is_nil(i)
+		assert.is_nil(t)
+	end)
+
+
 	it("over a single series", function()
 		local sr = {
 			series={
