a80f0cd765dbb9bff939ee9d977e3bd270693c43
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-07-05 10:26:09 -0500

    Drop a bit of debugging code.

--- tsq.lua
+++ tsq.lua
@@ -551,21 +551,4 @@ function TSQ.series_ipairs(series)
 	return TSQ.next_in_series, series, nil
 end
 
-------------------------------------------------------------------------------
----
--- Dump a table as a string; recursively
--- \returns string
-function dump_table(o)
-    if type(o) == 'table' then
-        local s = '{ '
-        for k,v in pairs(o) do
-            if type(k) ~= 'number' then k = '"'..k..'"' end
-            s = s .. '['..k..'] = ' .. dump_table(v) .. ','
-        end
-        return s .. '} '
-    else
-        return tostring(o)
-    end
-end
-
 -- vim: set ai sw=4 ts=4 :
